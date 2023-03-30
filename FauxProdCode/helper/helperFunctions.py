bb6aa5320f35d7b97fe9b22bcb95c7f1fa17ff6f
wischmcj <wischmcj@miamioh.edu>
2023-03-30 12:57:29 -0400

    Adding final touches to network processor, helper functions

--- FauxProdCode/helper/helperFunctions.py
+++ FauxProdCode/helper/helperFunctions.py
@@ -6,6 +6,45 @@ import os as os
 import numpy as np
 import networkx as nx
 import csv
+from . import globals 
+
+from uszipcode import SearchEngine, SimpleZipcode, ComprehensiveZipcode
+import requests, json
+
+api_key = globals.google_api_key
+ 
+# Take source as input
+source = input()
+ 
+# Take destination as input
+dest = input()
+ 
+# url variable store url
+url ='https://maps.googleapis.com/maps/api/distancematrix/json?'
+ 
+# Get method of requests module
+# return response object
+r = requests.get(url + 'origins = ' + source +
+                   '&destinations = ' + dest +
+                   '&key = ' + api_key)
+                    
+# json method of response object
+# return json format result
+x = r.json()
+
+def get_city(zip):
+    try:
+        search = SearchEngine()
+        return search.by_zipcode(int(zip)).major_city
+    except AttributeError:
+        return "Not Found"
+
+def get_state(zip):
+    try:
+        search = SearchEngine()
+        return search.by_zipcode(int(zip)).state
+    except AttributeError:
+        return "Not Found"
  
 def get_lat_lon(zip):
     uri = 'https://public.opendatasoft.com/api/records/1.0/search/?q={zip}&dataset=us-zip-code-latitude-and-longitude'
@@ -15,9 +54,7 @@ def rad(x):
   return x * math.pi / 180.0;
 
 
-def getHaversineDistance(zip1, zip2):
-  p1 = get_lat_lon(zip1)
-  p2 = get_lat_lon(zip2)
+def getHaversineDistance(p1, p2):
   R = 6378137; # Earth’s mean radius in meter
   dLat = rad(p2[0] - p1[0]);
   dLong = rad(p2[1] - p1[1]);
@@ -27,5 +64,3 @@ def getHaversineDistance(zip1, zip2):
   c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
   d = R * c #
   return d # // returns the distance in meter
-def Meters2Miles(meters):
-    return meters * 0.000621371
--- FauxProdCode/network_functions/network_data_processor.py
+++ FauxProdCode/network_functions/network_data_processor.py
@@ -12,6 +12,7 @@ from uszipcode import SearchEngine, SimpleZipcode, ComprehensiveZipcode
 
 def create_standard_file(start,end,package_volume,origin_dest_type='zip'):
     ##Extract needed info from the file provided, put in needed format
+    return
 
 #create a graph model of the network represente by the orders provided
 #weights determine the significance of results: distance, orders per month, travel time 
@@ -24,13 +25,30 @@ def create_network_graph(pld, weight_column):
     G = nx.from_pandas_edgelist(edgelist,source="source",target = "target",edge_attr = "weight" )
 
 def basic_net_stats():    
-    print("Eccentricity: ", nx.eccentricity(G))
-    print("Diameter: ", nx.diameter(G))
-    print("Radius: ", nx.radius(G))
-    print("Preiphery: ", list(nx.periphery(G)))
-    print("Center: ", list(nx.center(G)))
+     G_stats = pd.DataFrame(
+            {"Eccentricity:" nx.eccentricity(G)
+                "Diameter": nx.diameter(G),
+                "Radius": nx.radius(G),
+                "Preiphery": list(nx.periphery(G)),
+                "Center": list(nx.center(G)),
+                "degrees_in" = [d for n, d in G.in_degree()]
+                "degrees_out" = [d for n, d in G.out_degree()]
+                }
 
-def find_most_connected_hubs(num_hubs, )
-    #
+def find_most_connected_hubs(num_hubs )
+    #Returs a subgraph of the most connected components 
+    Gcc = sorted(nx.weakly_connected_components(G), key=len, reverse=True)
+    G0 = G.subgraph(Gcc[0:num_hubs])
 
+def draw_graph(G, pos):
+    nx.draw_networkx_edges(G, pos = pos, edge_color="r", width=1.0, alpha=0.5, arrows = False)
 
+def node_stats(G):    
+    nnodes = G.number_of_nodes()
+    degrees_in = [d for n, d in G.in_degree()]
+    degrees_out = [d for n, d in G.out_degree()]
+    avrg_degree_in = sum(degrees_in) / float(nnodes)
+    avrg_degree_out = sum(degrees_out) / float(nnodes)
+
+def map_graph(g):
+  # USE PLt and basemap to draw on map of the us 
